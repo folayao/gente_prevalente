0f222093f942e8f798a27bf398e46a0fd05fba5b
Felipe Olaya Ospina <folayao@eafit.edu.co>
2021-02-06 10:10:46 -0500

    The header is complete. Only is less of Link components to the routes.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 
 
 /* Styles */
+import lupa from '../assets/static/icons/lupa.png'
+import engranajes from '../assets/static/icons/engranajes.png'
+import cv from '../assets/static/icons/cv.png'
+import empleo from '../assets/static/icons/empleo.png'
+import elipse from '../assets/static/icons/elipse.png'
+import menu from '../assets/static/icons/menu.png'
+import dropdown from '../assets/static/icons/togglerDropdown.png'
 import logo from '../assets/static/logo1.png'
 import '../assets/styles/components/Header.scss'
 export default class Header extends Component {
@@ -10,21 +17,22 @@ export default class Header extends Component {
         return (
             <div className="navbar navbar-expand-md navbar-dark navbar__container" style={{ paddingTop: 0, paddingBottom: 0 }}>
                 <a className="navbar__container--title" href="#" style={{ paddingTop: 0, paddingBottom: 0 }}><img src={logo} className="" alt="Logo Gente Prevalente" /> Gente Prevalente</a>
-                <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation"></button>
-                <div className="collapse navbar-collapse" id="collapsibleNavId">
-                    <form className="form-inline ml-sm-5 ml-md-5 ml-0 mt-3 mt-sm-0 mt-md-0 my-lg-0">
-                        <input className="form-control mr-sm-2" style={{paddingLeft:0}}
+                <button className="navbar-toggler d-lg-none" style={{ border: 0 }} type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                    aria-expanded="false" aria-label="Toggle navigation"><img src={menu} alt="" /></button>
+                <div className="collapse navbar-collapse ml-sm-5 ml-md-5 " id="collapsibleNavId">
+                    <img src={lupa} alt="" className="pr-1" />
+                    <form className="form-inline  ml-0 mt-3 mt-sm-0 mt-md-0 my-lg-0">
+                        <input className="form-control mr-sm-2" style={{ paddingLeft: 0 }}
                             type="text" placeholder="Search..." id="form-control-input" />
-                        {/* <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
                     </form>
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Administración <span className="sr-only">(current)</span></a>
+                    <ul className="navbar-nav flex-md-row w-100 mt-2 ">
+                        <li className="nav-item active ml-md-auto  ">
+                            <a className="nav-link" href="#"> <img style={{ paddingBottom: 5 }} src={engranajes} alt="" /> Administración <span className="sr-only">(current)</span></a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Empleo
+
+                        <li className="nav-item dropdown ml-md-auto  active">
+                            <a className="nav-link  " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img style={{ paddingBottom: 5 }} src={empleo} alt="" /> Empleo <img src={dropdown} alt="" />
                             </a>
                             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <a className="dropdown-item" href="#">Action</a>
@@ -33,18 +41,17 @@ export default class Header extends Component {
                                 <a className="dropdown-item" href="#">Something else here</a>
                             </div>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Mi Cv</a>
+
+                        <li className="nav-item ml-md-auto  active ">
+                            <a className="nav-link " href="#"> <img src={cv} alt="" style={{ paddingBottom: 5 }} /> Mi Cv</a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Felipe
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+
+                        <li className="nav-item dropdown ml-md-auto  active ">
+                            <a className="nav-link " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src={elipse} alt="" style={{ paddingBottom: 3 }} /> Felipe <img src={dropdown} alt="" /></a>
+                            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Something else here</a>
+                                <a className="dropdown-item" href="#">Another</a>
                             </div>
                         </li>
                     </ul>
